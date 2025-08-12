8991bfc4ffa42f3b42e1ad351d15412169153743
Vasisthayadav2123 <vasisthayadav58@gmail.com>
2025-08-12 15:00:10 +0530

    file name casing fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Search from './Search';
-import FileUpLoader from './FileUploader';
+import FileUploader from './FileUploader';
 import { signOut } from '@/lib/actions/users.action';
 
 
@@ -12,7 +12,7 @@ const Header = ({userId , accountId}: {userId: string , accountId:string}) => {
         <Search />
 
         <div className='header-wrapper'>
-            <FileUpLoader ownerId={userId} accountId={accountId} />
+            <FileUploader ownerId={userId} accountId={accountId} />
 
             <form action={async () => {
                 'use server';
